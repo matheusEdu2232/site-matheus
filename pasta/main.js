42067076697579827a911a23850221ef59dcb10f
matheusEdu2232 <108136977+matheusEdu2232@users.noreply.github.com>
2024-09-27 17:42:05 -0300

    Add files via upload

---
+++ pasta/main.js
@@ -0,0 +1,17 @@
+<body>
+    <header>
+        <h1>Relatório das redes sociais</h1>
+        <nav>
+            <a href="index.html">Mundo</a>
+            <a href="#">Minha escola</a>
+        </nav>
+    </header>
+    <main class="graficos-section">
+        <section id="graficos-container" class="graficos-container">
+           
+        </section>
+    </main>
+    <footer>
+        <p>Desenvolvido por matheus</p>
+    </footer>
+</body>
